303c683b6069b0a2e554df47a0436bc736fc43c1
Anant <anant312004@gmail.com>
2021-11-28 23:43:02 +0530

    Added new file.

---
+++ Attrib_label_pack.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.geometry("500x500")
+root.title("Tere bhai ki GUI")
+
+# important label options
+# text - adds the text
+# bd - background
+# fg - foreground
+# font - sets the font
+# padx - x padding
+# pady - y padding 
+# relief - border styling - SUNKEN, RAISED, GROOVE, RIDGE
+
+title_label 
+root.mainloop()
