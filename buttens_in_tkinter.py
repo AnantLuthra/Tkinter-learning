b7b8c2d847c607d177c9da6ff3e15031d8ab564e
Anant <anant312004@gmail.com>
2021-12-23 00:17:39 +0530

    Added new files and learned entry_widget and grid layout in tkinter

--- buttens_in_tkinter.py
+++ buttens_in_tkinter.py
@@ -4,7 +4,6 @@ Date - 21/12/21
 Purpose - Exploring buttons in Tkinter, and making a GUI to tell 
 """
 from tkinter import *
-from tkinter import font
 import wikipedia
 
 def search_wiki(command):
@@ -20,31 +19,48 @@ def search_wiki(command):
 def print_command():
     print("Hello world")
 
+# Arguments of buttons
+# - (frame_name, fg=text_color, font=same as label, command=name of function)
+
+# How to pass arguments required by the provided fuction for the button.
+# We do it though lambda function
+
+# button1 = Button(frame1, text="Submit", command= lambda: name_of_function("Anant"))
 
 root = Tk()
 root.title("Exploring buttons in Tkinter")
 root.geometry("700x600")
 root.minsize("300", "300")
+
+# Frames
+
+
 frame1 = Frame(root, borderwidth=6, bg= "Light green", relief=SUNKEN)
 frame2 = Frame(root, borderwidth=40, bg= "sky blue")
+
+# Labels
+
 lable1 = Label(frame1, text="Press the button of required person's wikipedia",
  bg="Light green", fg="Black", font="comicsans 22 italic")
+
+# Buttons
+
 b3 = Button(frame2, text="Sharukh Khan", fg="brown", font="Arial 10 italic",
  command=lambda : search_wiki("sharukh khan"))
 b4 = Button(frame2, text="Narendra Modi", fg="brown", font="Arial 10 italic",
  command=lambda : search_wiki("Narendra Modi"))
 b2 = Button(frame1, text="Print \"Hello world\"", fg="brown",
  font="Arial 10 italic", command=print_command)
-close_button = Button(frame1, text="Close", bg="Black", fg="White", font="comicsans 15 bold",
- command=exit, pady=1)
+close_button = Button(frame1, text="Close", bg="Pink", fg="Black", font="comicsans 15 bold",
+ command=exit, pady=-20, padx=35)
 
 frame1.pack(side=TOP, fill=X)
 frame2.pack(side=TOP, fill=X, pady=20)
 close_button.pack(anchor="ne", padx=10)
 lable1.pack(fill=X)
-close_button.pack(side=TOP)
+close_button.pack(side=TOP, fill=X)
 b4.pack(side=TOP)
 b3.pack()
-
+b2.pack()
 
 root.mainloop()
---
+++ entery_widget_and_grid_layout.py
@@ -0,0 +1,59 @@
+"""
+Author - Anant luthra
+Date - 22/12/21
+Purpose - To learn and practice entery widget and grid layout in tkiter.
+"""
+
+from tkinter import *
+root = Tk()
+root.geometry("500x300")
+root.title("Tell your name")
+def submit_kr():
+    print(f"Your name is {main_name_entery.get()}")
+    print(f"and your sir name is {sir_name_entery.get()}")
+
+
+
+name = Label(root, text="Enter your name and sir name\nin the following boxes",
+ font="arial 20 italic",
+fg="Green", bg="cyan", padx=80)
+
+# lable_name.grid(row=1, column=2) -> Another way to showing a lable or etc on our GUI window
+# This function is like pack function but this works like cells in excel
+# like their are rows and coloumn in excel and similiar concept applies here.
+name.grid(column=2)
+
+
+# ----------------------------------------------------------------------------------------#
+
+# Variable classes in tkinter
+# StringVar, IntVar, BooleanVar, DoubleVar 
+
+# String_Var 
+
+main_name = StringVar()
+sir_name = StringVar()
+
+main_name_entery = Entry(root, textvariable= main_name, width=20)
+sir_name_entery = Entry(root, textvariable= sir_name, width=20)
+
+
+# arguments of .grid() function. ---------------------------------------------------------# 
+
+# - row=10 .. same as MS Excel
+# - column=10 .. same as MS Excel
+# - ipadx and ipady = inner size of entry box
+# - padx and pady = spaces to leave from x and y axis of the label.
+
+
+main_name_entery.grid(row=6, column=2, pady=10, ipadx=20, ipady=16)
+sir_name_entery.grid(row=8, column=2,pady=10, ipadx=20, ipady=16)
+
+Button(root, text="OK", font="comicsans 20 italic", bg="Green",
+ fg="white", relief=SUNKEN, command=submit_kr).grid(row=10, column=2)
+
+
+root.mainloop()
+
+
+#-----------------------------------------------------------------------------------------#
