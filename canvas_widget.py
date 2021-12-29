0abf8b430e6afd42cb4659b565bd3eeb78b1b3de
Anant <anant312004@gmail.com>
2021-12-29 23:12:46 +0530

    Learned create oval function is canvas widget.

--- canvas_widget.py
+++ canvas_widget.py
@@ -17,10 +17,12 @@ root.geometry(f"{canvas_width}x{canvas_height}")
 canvas_widget = Canvas(root, width=canvas_width, height=canvas_height, bg="light yellow")
 canvas_widget.pack()
 
-x1 = 0
-y1 = 0
-x2 = 600
-y2 = 600
+# can pass values from variable's like this.
+
+# x1 = 0
+# y1 = 0
+# x2 = 600
+# y2 = 600
 
 # For creating a line in our canvas we have to give x1 and y1 and x2 and y2 of two points in which between
 # the line will be created.
@@ -38,7 +40,7 @@ y2 = 600
 # 8                                                           |
 # 9                                                           |
 # -------------------------------------------------------------
-canvas_widget.create_line(x1, y1, x2, y2, fill="Blue")
+canvas_widget.create_line(0, 0, 600, 600, fill="Blue")
 canvas_widget.create_line(600, 0, 0, 600, fill="Blue")
 canvas_widget.create_line(0, 300, 600, 300, fill="Blue")
 canvas_widget.create_line(300, 0, 300, 600, fill="Blue")
@@ -83,8 +85,19 @@ canvas_widget.create_rectangle(285, 285, 315, 315, fill="light blue")
 
 # Creating text in our canvas .. for that we have to give the center coordinates of text.
 
-canvas_widget.create_text(300, 25, text="3D ILLISION", font="comicsans 30 italic")
+canvas_widget.create_text(300, 25, text="3D ILLISION", font=("footlight mt light", 30 ,"bold"))
 
 # Now creating an oval through .create_oval(x1, y1, x2, y2)
+
+canvas_widget.create_oval(60, 60, 540, 540)
+canvas_widget.create_oval(160, 160, 440, 440)
+canvas_widget.create_oval(210, 210, 390, 390)
+canvas_widget.create_oval(110, 110, 490, 490)
+canvas_widget.create_oval(260, 260, 340, 340)
+
+
+# Exploring another create functions of canvcas widget.
+
+# canvas_widget.create_polygon(60, 60, fill="green")
 
 root.mainloop()
