9d9fb9ee4ceb3743d9a6b630c6b7c10a332b80c8
Anant <anant312004@gmail.com>
2022-01-14 23:23:20 +0530

    Learned more in OOPs using in tkinter.

--- oops_in_tkinter.py
+++ oops_in_tkinter.py
@@ -8,24 +8,27 @@ from tkinter import *
 
 class GUI(Tk):
 
-    # def __init__(self):
-        # super().__init__()
-        # self.geometry("500x400")
-        # self.title("OOPs made GUI.")
-        # self.config = ("red")
-
-    def __init__(self, size, title, config: tuple):
+    def __init__(self):
         super().__init__()
-        print(f"size is-{size}, title is-{title}, config is-{config}")
-        self.geometry = size
-        self.title = title
-        self.config = config[0]
-        
-    def button_creator(self, text, font, command):
-        self = Button()
+        self.geometry("500x400")
+        self.title("Welcome screen")
+        self.config(bg="#bbf0af")
+    
+    def status_bar(self):
+        self.var = "Ready"
+        self.main_kuch = Label(self, text=self.var, anchor="w", relief=SUNKEN, font="arial", padx=7).pack(side=BOTTOM, fill=X)
+
+    def button_creator(self, text1):
+        Button(self, text=text1, font="comicsans 15"
+        , command = lambda: print("Command runned successfuly")).pack()
+    
+    def label_adder(self, text1):
+        Label(self, text = text1, font="arial").pack()
+
 
 if __name__ == '__main__':
-    # window = GUI()
-    window = GUI("500x400", "Welcome", ("red"))
-
+    window = GUI()
+    window.button_creator("Click here")
+    window.status_bar()
+    window.label_adder("Ban gaya dekh lo")
     window.mainloop()
