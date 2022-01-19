a48cb08fdcf0e8f37563e6e604658e75d842e43e
Anant <anant312004@gmail.com>
2022-01-19 20:36:47 +0530

    Completed designing part of calculator's GUI

--- Tkinter_exercises/calculator_gui.py
+++ Tkinter_exercises/calculator_gui.py
@@ -10,16 +10,12 @@ class Calculator(Tk):
 
     def __init__(self):
         super().__init__()
-        self.geometry("380x470")
+        self.geometry("358x447")
+        self.minsize("358", "447")
+        self.maxsize("358", "447")
         self.title("Calculator - By Anant Luthra")
         self.wm_iconbitmap("calculator.ico")
         self.config(bg="#13aaeb")
-
-    def frame(self):
-        "This function is made for making frames in which our calculator's buttons will be placed."
-
-        frame1 = Frame(self, height=100, width=380)
-        frame1.pack(fill=X)
 
     def give(self, digit):
         "This function is used to handle pressed button action. wheather it's a digit or a function button"
@@ -27,9 +23,12 @@ class Calculator(Tk):
         if digit == "=":
             value = eval(self.value.get())
         elif digit == "C":
-            value = ""
+            value = "0"
         else:
-            value = self.value.get() + digit
+            if self.value.get() == "0":
+                value = digit
+            else:
+                value = self.value.get() + digit
 
         self.value.set(value)
         self.update()
@@ -37,77 +36,114 @@ class Calculator(Tk):
     def entry(self):
         "This function is used to make entry widget for visible calculations."
         self.value = StringVar()
-        self.value.set("")
+        self.value.set("0")
         self.entery = Entry(self, font=("Bahnschrift Light ", 25), textvariable=self.value, bg="#b7dbeb")
-        self.entery.pack(fill=X, anchor="n", ipady=10, pady=5, padx=4)
+        self.entery.pack(fill=X, anchor="n", ipady=10, pady=7, padx=4)
 
     def all_buttons(self):
         
-        self.frame1 = Frame(self, height=100, width=380, bg="")
+        self.frame1 = Frame(self, height=100, width=380, bg="", padx=2)
         
+        # sign C
+        self.button = Button(self.frame1, text="C", command=lambda: self.give("C"), font=("cambria", 20), bg="#49b7e6",
+                               pady=10, padx=28, activebackground="#91cce6")
+        self.button.grid(column=4, row=1)
+
+        # sign /
+        self.button = Button(self.frame1, text="/", command=lambda: self.give("/"), font=("cambria", 20), bg="#49b7e6",
+                               pady=10, padx=26, activebackground="#91cce6")
+        self.button.grid(column=3, row=1)
+
+        # sign **
+        self.button = Button(self.frame1, text="**", command=lambda: self.give("**"), font=("cambria", 20), bg="#49b7e6",
+                               pady=10, padx=21, activebackground="#91cce6")
+        self.button.grid(column=2, row=1)
+
+        # sign *                                                                                                    
+        self.button = Button(self.frame1, text="*", command=lambda: self.give("*"), font=("cambria", 20), bg="#49b7e6",
+                               pady=10, padx=27, activebackground="#91cce6")
+        self.button.grid(column=1, row=1)
+
         # button 9
-        self.button = Button(self.frame1, text="9", command=lambda: self.give("9"), font=("cambria", 25),
-                             bg="#75c4e6", activebackground="#91cce6", pady=13, padx=14)
-        self.button.grid(padx=2, column=1, row=1)
+        self.button = Button(self.frame1, text="9", command=lambda: self.give("9"), font=("cambria", 20),
+                             bg="#75c4e6", activebackground="#91cce6", pady=10, padx=25)
+        self.button.grid(column=1, row=2)
 
         # button 8
-        self.button = Button(self.frame1, text="8", command=lambda: self.give("8"), font=("cambria", 25),
-                             bg="#75c4e6", activebackground="#91cce6",  pady=13, padx=14)
-        self.button.grid(padx=2, column=2, row=1)
+        self.button = Button(self.frame1, text="8", command=lambda: self.give("8"), font=("cambria", 20),
+                             bg="#75c4e6", activebackground="#91cce6",  pady=10, padx=25)
+        self.button.grid(column=2, row=2)
 
         # button 7
-        self.button = Button(self.frame1, text="7", command=lambda: self.give("7"), font=("cambria", 25),
-                             bg="#75c4e6", activebackground="#91cce6", pady=13, padx=14)
-        self.button.grid(padx=2, column=3, row=1)
+        self.button = Button(self.frame1, text="7", command=lambda: self.give("7"), font=("cambria", 20),
+                             bg="#75c4e6", activebackground="#91cce6", pady=10, padx=25)
+        self.button.grid(column=3, row=2)
 
         # sign %
-        self.button = Button(self.frame1, text="%", command=lambda: self.give("%"), font=("cambria", 25), bg="#49b7e6", pady=20, padx=12)
-        self.button.grid(padx=2, column=4, row=1)
+        self.button = Button(self.frame1, text="%", command=lambda: self.give("%"), font=("cambria", 20), bg="#49b7e6",
+                               pady=10, padx=23, activebackground="#91cce6")
+        self.button.grid(column=4, row=2)
 
         # button 6
-        self.button = Button(self.frame1, text="6", command=lambda: self.give("6"), font=("cambria", 25),
-                             bg="#75c4e6", activebackground="#91cce6", pady=13, padx=14)
-        self.button.grid(padx=2, column=1, row=2)
+        self.button = Button(self.frame1, text="6", command=lambda: self.give("6"), font=("cambria", 20),
+                             bg="#75c4e6", activebackground="#91cce6", pady=10, padx=25)
+        self.button.grid(column=1, row=3)
 
         # button 5
-        self.button = Button(self.frame1, text="5", command=lambda: self.give("5"), font=("cambria", 25),
-                             bg="#75c4e6", activebackground="#91cce6", pady=13, padx=14)
-        self.button.grid(padx=2, column=2, row=2)
+        self.button = Button(self.frame1, text="5", command=lambda: self.give("5"), font=("cambria", 20),
+                             bg="#75c4e6", activebackground="#91cce6", pady=10, padx=25)
+        self.button.grid(column=2, row=3)
 
         # button 4
-        self.button = Button(self.frame1, text="4", command=lambda: self.give("4"), font=("cambria", 25),
-                             bg="#75c4e6", activebackground="#91cce6", pady=13, padx=14)
-        self.button.grid(padx=2, column=3, row=2)
+        self.button = Button(self.frame1, text="4", command=lambda: self.give("4"), font=("cambria", 20),
+                             bg="#75c4e6", activebackground="#91cce6", pady=10, padx=25)
+        self.button.grid(column=3, row=3)
 
         # sign -
-        self.button = Button(self.frame1, text="-", command=lambda: self.give("-"), font=("cambria", 25), bg="#49b7e6", pady=20, padx=24)
-        self.button.grid(padx=1, column=4, row=2)
+        self.button = Button(self.frame1, text=" - ", command=lambda: self.give("-"), font=("cambria", 20), bg="#49b7e6",
+                              pady=10, padx=25, activebackground="#91cce6")
+        self.button.grid(column=4, row=3)
 
         # button 3
-        self.button = Button(self.frame1, text="3", command=lambda: self.give("3"), font=("cambria", 25),
-                             bg="#75c4e6", activebackground="#91cce6", pady=13, padx=14)
-        self.button.grid(padx=2, column=1, row=3)
+        self.button = Button(self.frame1, text="3", command=lambda: self.give("3"), font=("cambria", 20),
+                             bg="#75c4e6", activebackground="#91cce6", pady=10, padx=25)
+        self.button.grid(column=1, row=4)
 
         # button 2
-        self.button = Button(self.frame1, text="2", command=lambda: self.give("2"), font=("cambria", 25),
-                             bg="#75c4e6", activebackground="#91cce6", pady=13, padx=14)
-        self.button.grid(padx=2, column=2, row=3)
+        self.button = Button(self.frame1, text="2", command=lambda: self.give("2"), font=("cambria", 20),
+                             bg="#75c4e6", activebackground="#91cce6", pady=10, padx=25)
+        self.button.grid(column=2, row=4)
 
         # button 1
-        self.button = Button(self.frame1, text="1", command=lambda: self.give("1"), font=("cambria", 25),
-                             bg="#75c4e6", activebackground="#91cce6", pady=13, padx=14)
-        self.button.grid(padx=2, column=3, row=3)
+        self.button = Button(self.frame1, text="1", command=lambda: self.give("1"), font=("cambria", 20),
+                             bg="#75c4e6", activebackground="#91cce6", pady=10, padx=25)
+        self.button.grid(column=3, row=4)
 
         # sign +
-        self.button = Button(self.frame1, text="+", command=lambda: self.give("+"), font=("cambria", 25), bg="#49b7e6", pady=13, padx=14)
-        self.button.grid(padx=2, column=4, row=3)
+        self.button = Button(self.frame1, text="+ ", command=lambda: self.give("+"), font=("cambria", 20), bg="#49b7e6",
+                             pady=10, padx=25, activebackground="#91cce6")
+        self.button.grid(column=4, row=4)
 
         # button 0
-        self.button = Button(self.frame1, text="0", command=lambda: self.give("0"), font=("cambria", 25),
-                             bg="#75c4e6", activebackground="#91cce6", pady=13, padx=14)
-        self.button.grid(padx=2, column=2, row=4)
+        self.button = Button(self.frame1, text="0", command=lambda: self.give("0"), font=("cambria", 20),
+                             bg="#75c4e6", activebackground="#91cce6", pady=10, padx=25)
+        self.button.grid(column=2, row=5)
 
+        # sign .
+        self.button = Button(self.frame1, text=" . ", command=lambda: self.give("."), font=("cambria", 20), bg="#49b7e6",
+                             pady=10, padx=25, activebackground="#91cce6")
+        self.button.grid(column=3, row=5)
 
+        # sign =
+        self.button = Button(self.frame1, text="= ", command=lambda: self.give("="), font=("cambria", 20), bg="#49b7e6",
+                             pady=10, padx=25, activebackground="#91cce6")
+        self.button.grid(column=4, row=5)
+
+        # sign //
+        self.button = Button(self.frame1, text="//", command=lambda: self.give("//"), font=("cambria", 19), bg="#49b7e6",
+                               pady=12, padx=21, activebackground="#91cce6")
+        self.button.grid(column=1, row=5)
+        
         self.frame1.pack(fill=X, side=TOP)
 
 
@@ -117,12 +153,4 @@ if __name__ == "__main__":
     window.entry()
     window.all_buttons()
 
-    # window.functions("+")
-    # window.functions("-")
-    # window.functions("%")
-    # window.functions("*")
-    # window.functions("**")
-    # window.functions("/")
-    # window.functions("=")
-    # window.functions("C")
     window.mainloop()
